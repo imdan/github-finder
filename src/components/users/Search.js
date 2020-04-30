01f58b94f619025e99295762a97945bdc4295019
Dan <dmcg926@gmail.com>
2020-04-30 11:19:05 -0500

    added button to clear users from state, and an alert component to show when no input is submitted

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,24 +9,38 @@ export class Search extends Component {
     };
 
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
     };
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' });
+
+        if (this.state.text === '') {
+            this.props.setAlert('Please enter something', 'light');
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        }
+
     };
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
     render() {
+
+        const { showClear, clearUsers } = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className='form'>
                     <input type="text" name='text' placeholder='Search Users...' value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value='search' className='btn btn-dark btn-block'/>
                 </form>
+                {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>clear</button>}
+                
             </div>
         )
     }
